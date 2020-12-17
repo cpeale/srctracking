cd6b72dcfbdb87f4123c89e1faf015c1da9e469c
Charlotte Peale <charlotte.peale@gmail.com>
2020-12-17 07:24:02 -0800

    different bench grouping

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -725,13 +725,248 @@ fn scheme_2(c: &mut Criterion) {
 
     }
     group.finish();
-    //sender time (user)
-
-    //platform time (overall)
-
-    //receiving time (user)
 }
 
-criterion_group!(benches, scheme_1, scheme_2);
+fn scheme_2_revised(c: &mut Criterion) {
+    let sizes = [10, 100, 200, 500, 800, 1000, 2000, 5000, 8000];
+
+    let mut group = c.benchmark_group("scheme 2");
+    for size in sizes.iter() {
+        group.bench_with_input(BenchmarkId::new("author and receive", size), size, |b, &size| {
+            b.iter_batched(|| {
+                //setup
+                let mut rng1 = OsRng::new().unwrap();
+                let mut plaintext1 = vec![0; size];
+                rng1.fill_bytes(&mut plaintext1);
+
+
+                let mut rng = OtherRng {};
+                let plat = S2Plat::new(&mut rng);
+                let (mut u1, mut u2) = S2User::new(&mut rng, &plat);
+                let pd = u1.author(&mut rng, &plat, b"test");
+                let (msg, fd) = u2.receive(pd, &plat, &mut rng);
+                
+                (u1, u2, plaintext1, rng, plat)
+            }
+            , 
+            |(mut u1, mut u2, plaintext1, mut rng, plat)| {
+                let pd = u2.author(&mut rng, &plat, &plaintext1);
+                let (msg, fd) = u1.receive(pd, &plat, &mut rng);
+            },
+            BatchSize::SmallInput
+        );
+        });
+
+        group.bench_with_input(BenchmarkId::new("forward and receive", size), size, |b, &size| {
+            b.iter_batched(|| {
+                //setup
+                let mut rng1 = OsRng::new().unwrap();
+                let mut plaintext1 = vec![0; size];
+                rng1.fill_bytes(&mut plaintext1);
+
+
+                let mut rng = OtherRng {};
+                let plat = S2Plat::new(&mut rng);
+                let (mut u1, mut u2) = S2User::new(&mut rng, &plat);
+                let pd = u1.author(&mut rng, &plat, &plaintext1);
+                let (msg, fd) = u2.receive(pd, &plat, &mut rng);
+                
+                (u1, u2, plaintext1, rng, plat, fd)
+            }
+            , 
+            |(mut u1, mut u2, plaintext1, mut rng, plat, fd)| {
+                let pd = u2.forward(&mut rng, &plat, &plaintext1, fd);
+                let (msg, fd) = u1.receive(pd, &plat, &mut rng);
+            },
+            BatchSize::SmallInput
+        );
+        });
+
+        group.bench_with_input(BenchmarkId::new("only author", size), size, |b, &size| {
+            b.iter_batched(|| {
+                //setup
+                let mut rng1 = OsRng::new().unwrap();
+                let mut plaintext1 = vec![0; size];
+                rng1.fill_bytes(&mut plaintext1);
+
+
+                let mut rng = OtherRng {};
+                let plat = S2Plat::new(&mut rng);
+                let (mut u1, mut u2) = S2User::new(&mut rng, &plat);
+                let pd = u1.author(&mut rng, &plat, b"test");
+                let (msg, fd) = u2.receive(pd, &plat, &mut rng);
+                
+                (u1, u2, plaintext1, rng, plat)
+            }
+            , 
+            |(mut u1, mut u2, plaintext1, mut rng, plat)| {
+                let pd = u2.author(&mut rng, &plat, &plaintext1);
+            },
+            BatchSize::SmallInput
+        );
+        });
+
+        group.bench_with_input(BenchmarkId::new("only forward", size), size, |b, &size| {
+            b.iter_batched(|| {
+                //setup
+                let mut rng1 = OsRng::new().unwrap();
+                let mut plaintext1 = vec![0; size];
+                rng1.fill_bytes(&mut plaintext1);
+
+
+                let mut rng = OtherRng {};
+                let plat = S2Plat::new(&mut rng);
+                let (mut u1, mut u2) = S2User::new(&mut rng, &plat);
+                let pd = u1.author(&mut rng, &plat, &plaintext1);
+                let (msg, fd) = u2.receive(pd, &plat, &mut rng);
+                
+                (u1, u2, plaintext1, rng, plat, fd)
+            }
+            , 
+            |(mut u1, mut u2, plaintext1, mut rng, plat, fd)| {
+                let pd = u2.forward(&mut rng, &plat, &plaintext1, fd);
+            },
+            BatchSize::SmallInput
+        );
+        });
+
+        group.bench_with_input(BenchmarkId::new("only receive (author)", size), size, |b, &size| {
+            b.iter_batched(|| {
+                //setup
+                let mut rng1 = OsRng::new().unwrap();
+                let mut plaintext1 = vec![0; size];
+                rng1.fill_bytes(&mut plaintext1);
+
+
+                let mut rng = OtherRng {};
+                let plat = S2Plat::new(&mut rng);
+                let (mut u1, mut u2) = S2User::new(&mut rng, &plat);
+                let pd = u1.author(&mut rng, &plat, b"test");
+                let (msg, fd) = u2.receive(pd, &plat, &mut rng);
+                let pd = u2.author(&mut rng, &plat, &plaintext1);
+
+                (u1, u2, rng, plat, pd)
+            }
+            , 
+            |(mut u1, mut u2, mut rng, plat, pd)| {
+                let (msg, fd) = u1.receive(pd, &plat, &mut rng);
+            },
+            BatchSize::SmallInput
+        );
+        });
+
+        group.bench_with_input(BenchmarkId::new("only receive (forward)", size), size, |b, &size| {
+            b.iter_batched(|| {
+                //setup
+                let mut rng1 = OsRng::new().unwrap();
+                let mut plaintext1 = vec![0; size];
+                rng1.fill_bytes(&mut plaintext1);
+
+
+                let mut rng = OtherRng {};
+                let plat = S2Plat::new(&mut rng);
+                let (mut u1, mut u2) = S2User::new(&mut rng, &plat);
+                let pd = u1.author(&mut rng, &plat, &plaintext1);
+                let (msg, fd) = u2.receive(pd, &plat, &mut rng);
+                let pd = u2.forward(&mut rng, &plat, &plaintext1, fd);
+                (u1, u2, rng, plat, pd)
+            }
+            , 
+            |(mut u1, mut u2, mut rng, plat, pd)| {
+                let (msg, fd) = u1.receive(pd, &plat, &mut rng);
+            },
+            BatchSize::SmallInput
+        );
+        });
+
+        group.bench_with_input(BenchmarkId::new("report", size), size, |b, &size| {
+            b.iter_batched(|| {
+                //setup
+                let mut rng1 = OsRng::new().unwrap();
+                let mut plaintext1 = vec![0; size];
+                rng1.fill_bytes(&mut plaintext1);
+
+
+                let mut rng = OtherRng {};
+                let plat = S2Plat::new(&mut rng);
+                let (mut u1, mut u2) = S2User::new(&mut rng, &plat);
+                let pd = u1.author(&mut rng, &plat, &plaintext1);
+                let (msg, fd) = u2.receive(pd, &plat, &mut rng);
+                (u1, u2, rng, plat, fd, plaintext1)
+            }
+            , 
+            |(mut u1, mut u2, mut rng, plat, fd, plaintext1)| {
+                let rep = u2.report(fd, &mut rng, &plat);
+                let uid = plat.validate_report(&plaintext1, rep);
+            },
+            BatchSize::SmallInput
+        );
+        });
+
+        group.bench_with_input(BenchmarkId::new("send, only user", size), size, |b, &size| {
+            b.iter_batched(|| {
+                //setup
+                let mut rng1 = OsRng::new().unwrap();
+                let mut plaintext1 = vec![0; size];
+                rng1.fill_bytes(&mut plaintext1);
+
+
+                let mut rng = OtherRng {};
+                let plat = S2Plat::new(&mut rng);
+                let (mut u1, mut u2) = S2User::new(&mut rng, &plat);
+                let pd = u1.author(&mut rng, &plat, b"test");
+                let (msg, fd) = u2.receive(pd, &plat, &mut rng);
+                
+                (u1, u2, plaintext1, rng, plat)
+            }
+            , 
+            |(mut u1, mut u2, plaintext1, mut rng, plat)| {
+                let pd = u2.author_user_only(&mut rng, &plat, &plaintext1);
+            },
+            BatchSize::SmallInput
+        );
+        });
+
+        group.bench_with_input(BenchmarkId::new("receive, only platform", size), size, |b, &size| {
+            b.iter_batched(|| {
+                //setup
+                let mut rng1 = OsRng::new().unwrap();
+                let mut plaintext1 = vec![0; size];
+                rng1.fill_bytes(&mut plaintext1);
+
+
+                let mut rng = OtherRng {};
+                let plat = S2Plat::new(&mut rng);
+                let (mut u1, mut u2) = S2User::new(&mut rng, &plat);
+                let pd = u1.author(&mut rng, &plat, b"test");
+                let out = u2.receive_send_only(pd, &plat, &mut rng);
+
+                let pk = out.1.decompress().unwrap();
+                let attr = ((out.2.0.0.decompress().unwrap(), out.2.0.1.decompress().unwrap()), 
+                (out.2.1.0.decompress().unwrap(), out.2.1.1.decompress().unwrap()), 
+                (out.2.2.0.decompress().unwrap(), out.2.2.1.decompress().unwrap()));
+                
+                (u1, u2, plaintext1, rng, plat, out, pk, attr)
+            }
+            , 
+            |(mut u1, mut u2, plaintext1, mut rng, plat, out, pk, attr)| {
+                plat.verify_receive(out.0, out.1, 
+                    out.2, 
+                    out.3, out.4, out.5, 
+                    out.6, out.7);
+        
+                let cert = plat.issue_cert(&mut rng, pk, 
+                attr);
+        
+            },
+            BatchSize::SmallInput
+        );
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, scheme_2_revised);
+//criterion_group!(benches, scheme_1, scheme_2);
 //criterion_group!(benches, scheme_2);
 criterion_main!(benches);
